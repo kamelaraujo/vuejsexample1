610d1e5c7aeb97c628add7b95b6c7d814ca7293a
kamelaraujo <kamel.araujo@gmail.com>
2019-11-13 16:07:53 -0500

    agrege nuevo campo cantidad a cada tipo de fruta y suma automatica con funcion computed

--- app.js
+++ app.js
@@ -3,19 +3,29 @@ const kamel = new Vue({
     data: {
         titulo: 'kamel es bueno',
         frutas: [
-            {nombre: 'pera', cantidad: 10},
+            {nombre: 'pera', cantidad: 0},
             {nombre: 'manzana', cantidad: 31},
             {nombre: 'naranja', cantidad: 45},
             {nombre: 'platano', cantidad: 7}
         ],
         nuevafruta:'',
-        nuevacantidad:''
+        total: 0
     },
     methods: {
         agregarfruta() {
             this.frutas.push({
-                nombre: this.nuevafruta, cantidad: this.nuevacantidad
-            })
+                nombre: this.nuevafruta, cantidad: 0
+            });
+            this.nuevafruta = '';
+        }
+    },
+    computed: {
+        sumarfrutas() {
+            this.total=0;
+            for(fruta of this.frutas){
+                this.total = this.total+fruta.cantidad;
+            }
+            return this.total;
         }
     }
 })
